066615cbb6440c6c98c4d2cb60fecc4ead95bb22
rlvy0513 <rlvy0513@naver.com>
2022-06-22 10:14:59 +0900

    stepgp1jstl

---
+++ src/main/java/notice/controller/customer/NoticeController.java
@@ -0,0 +1,44 @@
+package notice.controller.customer;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+
+import java.util.*;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import notice.controller.Controller;
+import notice.dao.NoticeDao;
+import notice.vo.Notice;
+
+public class NoticeController implements Controller {
+	@Override
+	public void execute(HttpServletRequest request, HttpServletResponse response) 
+			throws Exception {
+		System.out.println("NoticeController pass");
+
+		String field=request.getParameter("f");
+		System.out.println("field : "+field);
+		if(field==null || field.equals("")) {
+			field="title";
+		}
+		String query=request.getParameter("q");
+		if(query==null)
+			query="";
+		
+		//db접속해서 select 결과물을 가져오기
+		 NoticeDao dao=new NoticeDao();
+		 List<Notice> list=dao.getNotices(field,query);
+		 
+		//request에 Notice n을 담아주기
+		request.setAttribute("list", list);
+		request.setAttribute("query", query);
+		
+			
+		request.getRequestDispatcher("notice.jsp").forward(request, response);
+		
+	}
+	
+
+}
---
+++ src/main/webapp/jstlsample/jstl1_token.jsp
@@ -0,0 +1,13 @@
+<%@ page language="java" contentType="text/html; charset=UTF-8"
+    pageEncoding="UTF-8"%>
+<% request.setCharacterEncoding("utf-8"); %>
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="UTF-8">
+<title>jstl1_token</title>
+</head>
+<body>
+<h3>jstl1_token</h3>
+</body>
+</html>
